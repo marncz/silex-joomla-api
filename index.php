484a4e385e24c1d9edce385415ed77f493ceb54a
marncz <marnczarnecki@gmail.com>
2017-03-11 15:31:07 +0000

    skeleton

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
-// web/index.php
+define('_JEXEC', 1);
+define('JPATH_BASE', '../');
+require_once JPATH_BASE . 'includes/defines.php';
+require_once JPATH_BASE . 'includes/framework.php';
+
 require_once 'vendor/autoload.php';
 $app = new Silex\Application();
 
-// Import API's routes
+
+
 require_once 'routes.php';
 
 error_reporting(E_ALL);
